19150e051848b26970ecee32624323300362822b
alamine <alaminedione@proton.me>
2025-07-26 23:37:54 +0000

    add script

---
+++ install.sh
@@ -0,0 +1,6 @@
+NOTE: ce script permettra de configurer automatiquement les themes
+
+- faire les backup des configurations existantes dans ~/$HOME sous le format nom-theme-datetime.back 
+- cp les fichiers de configurations dans les repertoires appropriés
+- executer les scripts pour terminer la configuration . (parexemple les scripts pour telecharger les themes)
+- execute le script ./scripts/change-theme/set-mocha.sh pour charger le theme mocha qui est le theme par defaut
