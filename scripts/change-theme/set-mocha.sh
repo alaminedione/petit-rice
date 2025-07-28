94845991795b426b204fd8e79c5226a22c66435e
alamine <alaminedione@proton.me>
2025-07-28 10:45:38 +0000

    add script for changing themes

---
+++ scripts/change-theme/set-mocha.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# foot
+sed -i "s|^include=~/.config/foot/themes/.*|include=~/.config/foot/themes/catppuccin-macchiato.ini|" ~/.config/foot/foot.ini
+
+# kitty
+echo "include themes/macchiato.conf" > ~/.config/kitty/colors.conf
+
+# wofi
+cp ~/.config/wofi/macchiato.css ~/.config/wofi/style.css
+
+# waybar sway
+cp ~/.config/waybar/macchiato.css ~/.config/waybar/style.css
+
+# waybar hyprland
+cp ~/.config/hypr/waybar/macchiato.css ~/.config/hypr/waybar/style.css
+
+# vim
+sed -i "s|^set background=light|set background=dark|" ~/.vimrc
+sed -i "s|^colorscheme .*|colorscheme catppuccin_macchiato|" ~/.vimrc
+
+# nvim
+sed -i "s|theme = .*|theme = \"catppuccin\",|" ~/.config/nvim/lua/chadrc.lua
+nvim --headless +'lua require("base46").load_all_highlights()' +qa
+
+
+# sway
+sed -i "s|^include=./themes/.*|include=./themes/catppuccin-mocha|" ~/.config/sway/config
+
+# mako
+cp ~/.config/mako/macchiato ~/.config/mako/config
+
+# ghostty
+# sed -i "s|^theme=.*|theme=catppuccin-macchiato|" ~/.config/ghostty/config
+sed -i "/^theme=/s|.*|theme=catppuccin-macchiato|" ~/.config/ghostty/config
+
+# gsettings
+gsettings set org.gnome.desktop.interface icon-theme 'Luv'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+gsettings set org.gnome.desktop.interface font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface document-font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+gsettings set org.gnome.desktop.interface cursor-theme 'Layan-white Cursors'
+gsettings set org.gnome.desktop.interface cursor-size 24
+
+echo -e "[Settings] \n gtk-application-prefer-dark-theme=1" > ~/.config/gtk-3.0/settings.ini
+echo -e "[Settings] \n gtk-application-prefer-dark-theme=1" > ~/.config/gtk-4.0/settings.ini
+
+#  kvantum 
+sed -i "s|theme=.*|theme=KvArcDark|" ~/.config/Kvantum/kvantum.kvconfig
+
+# Fin
+echo "Thème Catppuccin Macchiato appliqué avec succès !"
+echo "Redémarrez vos applications pour voir les changements."
---
+++ scripts/change-theme/set-rose-pine-dawn.sh
@@ -0,0 +1,53 @@
+# changer le theme du terminal foot a catppuccin macchiato
+sed -i "s|^include=~/.config/foot/themes/.*|include=~/.config/foot/themes/rose-pine-dawn.ini|" ~/.config/foot/foot.ini
+# changer le theme du terminal kitty a catppuccin mocha 
+echo "include themes/rose-pine-dawn.conf" > ~/.config/kitty/colors.conf
+
+# changer le theme de wofi a catppuccin macchiato
+cp ~/.config/wofi/rose-pine-dawn.css ~/.config/wofi/style.css
+
+# waybar sway
+cp ~/.config/waybar/rose-pine-dawn.css ~/.config/waybar/style.css
+
+#waybar hyprland  
+cp ~/.config/hypr/waybar/rose-pine-down.css ~/.config/hypr/waybar/style.css
+
+# changer le theme de vim a macchiato
+sed -i "s|^set background=light|set background=dark|" ~/.vimrc
+sed -i "s|^colorscheme .*|colorscheme rosepine_dawn|" ~/.vimrc
+
+# changer le theme de nvim a catppuccin_macchiato 
+sed -i "s|theme = .*|theme = \"flexoki-light\",|" ~/.config/nvim/lua/chadrc.lua
+nvim --headless +'lua require("base46").load_all_highlights()' +qa
+
+# changer le theme de sway a catppuccin catppuccin_macchiato
+sed -i "s|^include=./themes/.*|include=./themes/rose-pine-dawn|" ~/.config/sway/config
+
+# changer le theme de mako 
+cp ~/.config/mako/rose-pine-dawn ~/.config/mako/config
+
+# ghostty
+# sed -i "s|theme=.*|theme=rose-pine-dawn|" ~/.config/ghostty/config
+sed -i "/^theme=/s|.*|theme=rose-pine-dawn|" ~/.config/ghostty/config
+
+# gsettings
+gsettings set org.gnome.desktop.interface icon-theme 'Luv'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+gsettings set org.gnome.desktop.interface gtk-theme 'Orchis-Light'
+gsettings set org.gnome.desktop.wm.preferences theme 'Orchis-Light'
+gsettings set org.gnome.desktop.interface font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface document-font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 10.4'
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+gsettings set org.gnome.desktop.interface cursor-theme 'Layan Cursors'
+gsettings set org.gnome.desktop.interface cursor-size 24
+
+echo "" > ~/.config/gtk-3.0/settings.ini
+echo  "" > ~/.config/gtk-4.0/settings.ini
+
+# changer le theme kvantum 
+sed -i "s|theme=.*|theme=KvArc|" ~/.config/Kvantum/kvantum.kvconfig
+
+
+echo "Thème rose-pine-dawn appliqué avec succès !"
+echo "Redémarrez vos applications pour voir les changements."
