429e78e435dcc3626528124c3d9944c0b3d4c524
alamine <alaminedione@proton.me>
2025-07-29 17:33:01 +0000

    ne necessite plus le lancement du script avec sudo

--- scripts/set-swapinness.sh
+++ scripts/set-swapinness.sh
@@ -2,12 +2,6 @@
 
 # Script to configure swappiness to 10 on Arch Linux
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or with sudo."
-  exit 1
-fi
-
 # Desired value
 SWAPPINESS_VALUE=10
 
@@ -16,9 +10,9 @@ sysctl -w vm.swappiness=$SWAPPINESS_VALUE
 
 # Create or modify the configuration file to make the change persistent
 CONF_FILE="/etc/sysctl.d/99-swappiness.conf"
-echo "vm.swappiness=$SWAPPINESS_VALUE" > "$CONF_FILE"
+echo "vm.swappiness=$SWAPPINESS_VALUE" | sudo tee "$CONF_FILE" > /dev/null
 
 # Apply all sysctl configurations
-sysctl --system
+sudo sysctl --system
 
 echo "Swappiness set to $SWAPPINESS_VALUE and made permanent via $CONF_FILE"
